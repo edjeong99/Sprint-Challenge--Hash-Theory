9f050fc30bf2f5c87e59058504a6c7bec1e47c4d
Edward J <edjeong99@gmail.com>
2019-03-29 12:03:41 -0400

    ex2 works

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -11,9 +11,20 @@ char **reconstruct_trip(Ticket **tickets, int length)
 
   // YOUR CODE HERE
 
-  return route;
-}
+  // add each ticket to hash table
+  for(int i = 0; i < length; i++){
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination );
+  }
 
+  char *source = "NONE";
+  
+  for(int i = 0; i < length; i++){
+    route[i] = hash_table_retrieve(ht,source);
+    source = route[i];
+}
+  return route;
+
+}
 void print_route(char **route, int length)
 {
   for (int i = 0; i < length; i++) {
